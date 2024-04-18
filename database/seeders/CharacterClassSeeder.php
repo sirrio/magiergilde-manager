ff27abe6ca2b6d46f5e4b8f0ab5078244c82c7b6
sirrio <home@sirrio.de>
2024-04-18 14:26:56 +0200

    singled out class seeder

--- database/seeders/CharacterClassSeeder.php
+++ database/seeders/CharacterClassSeeder.php
@@ -2,16 +2,38 @@
 
 namespace Database\Seeders;
 
+use App\Models\CharacterClass;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CharacterClassSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $classes = [
+      'Artificer' => '/images/artificer.jpeg',
+      'Barbarian' => '/images/barbarian.jpeg',
+      'Bard' => '/images/bard.jpeg',
+      'Blood Hunter' => '/images/bloodhunter.jpeg',
+      'Cleric' => '/images/cleric.jpeg',
+      'Druid' => '/images/druid.jpeg',
+      'Fighter' => '/images/fighter.jpeg',
+      'Monk' => '/images/monk.jpeg',
+      'Paladin' => '/images/paladin.jpeg',
+      'Ranger' => '/images/ranger.jpeg',
+      'Rogue' => '/images/rogue.jpeg',
+      'Sorcerer' => '/images/sorcerer.jpeg',
+      'Warlock' => '/images/warlock.jpeg',
+      'Wizard' => '/images/wizard.jpeg',
+    ];
+    foreach ($classes as $class => $src) {
+      CharacterClass::factory()->create([
+        'name' => $class,
+        'src' => $src
+      ]);
     }
+  }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,29 +23,5 @@ class DatabaseSeeder extends Seeder
       'email' => 'home@sirrio.de',
       'password' => Hash::make('test')
     ]);
-
-
-    $classes = [
-      'Artificer' => '/images/artificer.jpeg',
-      'Barbarian' => '/images/barbarian.jpeg',
-      'Bard' => '/images/bard.jpeg',
-      'Blood Hunter' => '/images/bloodhunter.jpeg',
-      'Cleric' => '/images/cleric.jpeg',
-      'Druid' => '/images/druid.jpeg',
-      'Fighter' => '/images/fighter.jpeg',
-      'Monk' => '/images/monk.jpeg',
-      'Paladin' => '/images/paladin.jpeg',
-      'Ranger' => '/images/ranger.jpeg',
-      'Rogue' => '/images/rogue.jpeg',
-      'Sorcerer' => '/images/sorcerer.jpeg',
-      'Warlock' => '/images/warlock.jpeg',
-      'Wizard' => '/images/wizard.jpeg',
-      ];
-    foreach ($classes as $class => $src) {
-      CharacterClass::factory()->create([
-        'name' => $class,
-        'src' => $src
-      ]);
-    }
   }
 }
